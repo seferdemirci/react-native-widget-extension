e24f80d9ea2d328d298d589ddc8dfcae9641c9fc
Sefer Demirci <63615719+seferdemirci@users.noreply.github.com>
2024-10-19 10:39:59 +0200

    Update index.d.ts

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,5 @@
 export declare function areActivitiesEnabled(): boolean;
+export declare function isActivityActive(): boolean;
 export declare function startActivity(...args: any): void;
 export declare function updateActivity(...args: any): void;
 export declare function endActivity(...args: any): void;
